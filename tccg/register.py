afdf3d84a240b156af07c8fb90d5b29b08a62c0f
FamALouiz <fam@awadlouis.com>
2026-06-09 21:53:06 +0200

    refactor: fix number of elements passed in as a float

--- tccg/register.py
+++ tccg/register.py
@@ -1,21 +1,21 @@
 # Copyright (C) 2016 Paul Springer (springer@aices.rwth-aachen.de) - All Rights Reserved
 import copy
 
+
 class Register:
     def __init__(self, name, numElements):
         self.name = name
-        self.numElements = numElements
-        self.content = ["" for i in range(numElements)]
+        self.numElements = int(numElements)
+        self.content = ["" for i in range(self.numElements)]
 
     def setzero(self):
         self.content = ["0" for i in range(self.numElements)]
 
     def __str__(self):
-        ret = "%s = ["%self.name
+        ret = "%s = [" % self.name
         for s in self.content:
             ret += s + ";"
-        return ret[:-1]+"]"
+        return ret[:-1] + "]"
 
     def setContent(self, content):
         self.content = copy.deepcopy(content)
-
